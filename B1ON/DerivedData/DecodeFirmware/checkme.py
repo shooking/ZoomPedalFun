bd8f97f4a130af6fd198bf97b7e1adb69d4a35b2
Stephen Hookings <stephen.hookings.1985@gmail.com>
2021-09-17 18:00:03 +0100

    Added Firmware decoder for ZDL files.

---
+++ B1ON/DerivedData/DecodeFirmware/checkme.py
@@ -0,0 +1,46 @@
+# -*- coding: ascii -*-
+import sys
+import json
+
+def check(name):
+    f = open(name, "rb")
+    f.read(64)
+    x = f.read(8)
+    data = f.read()
+    f.close()
+    # try to find name. Is is 0x30 chars from OnOff
+    OnOffstart = data.find("OnOff".encode())
+    fxName=""
+    for j in range(12):
+        if data[OnOffstart + j + 0x30] == 0x00:
+            break
+        fxName = fxName + chr(data[OnOffstart + j + 0x30])
+
+    #print( "{} {} {} {}".format( hex(x[0]), hex(x[1]), hex(x[2]), hex(x[3]) ) )
+    mult = 256
+    tD = {
+            "filename": name,
+            "fxname" :fxName
+         }
+    if x[2] & 0xC0 == 0:
+        tD['fxid'] = str(hex( x[1] * mult + x[0] ))
+        tD['gid'] = str(hex(x[3]) )
+        """
+        print("{} {}\t\t{} {} {}{}{}{}".format(name, fxName, str(hex( x[1] * mult + x[0] )), str(hex(x[3]) ),
+            chr(x[4]), chr(x[5]), chr(x[6]), chr(x[7])) )
+        """
+    else:
+        tD['fxid'] = str(hex( x[1] * mult + x[0] ))
+        tD['gid'] = str(hex( ( (x[3] * mult) + (x[2] & 0xC0)) >>4 ) )
+        """
+        print("{} {} \t\t{} {} {}{}{}{}".format(name, fxName, str(hex( x[1] * mult + x[0] )), str(hex(( (x[3] * mult) + (x[2] & 0xC0)) >> 4) ),
+            chr(x[4]), chr(x[5]), chr(x[6]), chr(x[7])) )
+        """
+    tD['version'] = "{}{}{}{}".format(chr(x[4]), chr(x[5]), chr(x[6]), chr(x[7]) )
+
+    json.dump(tD, sys.stdout, indent=4)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        check(sys.argv[1])
