2752d1248454d3f03fe13c6a549d7d3ff3d6b675
Stephen Hookings <stephen.hookings.1985@gmail.com>
2021-11-15 12:35:14 +0000

    Updated rhythm controls - need to choose slot!

--- G5n/RhythmOn.sh
+++ G5n/RhythmOn.sh
@@ -1,8 +1,10 @@
 export MIDI_DEV=`amidi -l | grep ZOOM | awk '{print $2}'`
-probeString="F0 52 00 6E 64 03 00 0A 05 01 00 00 00 00 F7"
+# you have to prime the rhythm then choose slot
+# optional - you can turn editor on it you want to see interaction.
+slot=$(($1-1))
+hexSlot=`printf "%02x" $slot`
+probeString="F0 52 00 6E 64 03 00 0A ${hexSlot} 01 00 00 00 00 F7"
 theFile=rhythmnOnOff.bin
 echo ${probeString}
 
 amidi -p ${MIDI_DEV} -S ${probeString} -r ${theFile} -t 1 ; hexdump -C ${theFile}
-
-
