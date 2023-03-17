7eb4a0e7396bcd0ac2658a81c6f5af83ef44ed80
Stephen Hookings <stephen.hookings@sap.com>
2023-03-17 11:53:26 +0000

    Adding an autobuild.sh syntax suggest by CHATGPT

--- autobuild.sh
+++ autobuild.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-cd ./B1ON
-make clean all
-cd ..
+#!/bin/bash # Define variables
+SRCDIR=./B1ON
+BINDIR=./B1ON
+CXX=g++
+CXXFLAGS="-Wall -Wextra -pedantic"
+${CXX} ${CXXFLAGS} ${SRCDIR}/B1OnPatchGetName.cpp -o ${BINDIR}/B1OnPatchGetName
+exit $?
